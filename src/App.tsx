b6dde56f0b0f494d78a0f56474d7eb99715e8436
Calychas <kacper.lesniara@gmail.com>
2021-01-14 21:40:23 +0100

    add flex to layout, so that it properly fills enitre page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import {
-  CssBaseline,
-} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -8,23 +7,43 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import styled from "styled-components";
 import { Home } from "./components/Home";
 import { MainBar } from "./components/MainBar";
+
+const GridAppBar = styled(Grid)`
+  flex: 0 0 auto
+`;
+
+const GridContent = styled(Grid)`
+  flex: 1 0 auto
+`;
 
 
 function App() {
   return (
     <Router>
       <CssBaseline />
-
-      <MainBar></MainBar>
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="stretch"
+        wrap="nowrap"
+        spacing={0}
+      >
+        <GridAppBar item>
+          <MainBar></MainBar>
+        </GridAppBar>
+        <GridContent item>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </GridContent>
+      </Grid>
     </Router>
   );
 }
